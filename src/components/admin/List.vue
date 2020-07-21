7fed3438df6f63135c9671353d59cd3278901bc2
mccrush <mccrush2027@gmail.com>
2020-07-21 21:46:00 +0700

    Settings Lists

---
+++ src/components/admin/List.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <button
+      v-for="i in 5"
+      :key="'bt'+i"
+      class="btn btn-sm btn-block btn-light border-0 rounded-0 m-0 text-left pl-3"
+    >Btn</button>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -9,8 +9,32 @@
           </div>
         </div>
         <div class="row">
-          <div class="col-3 col-md-2 border-right admin-sidebar">t</div>
-          <div class="col-3 col-md-2 border-right">e</div>
+          <div class="col-3 col-md-2 border-right p-0 admin-sidebar">
+            <h6 class="text-center m-0 mt-2 pb-2 border-bottom">
+              Коллекции
+              <button
+                v-show="true"
+                @click="createDoc = true; selectDocId=''; doc = {active: true, position: docs.length + 1}"
+                class="btn btn-sm btn-outline-primary p-0 pl-2 pr-2 ml-1 border-0"
+                :disabled="selectCollectionAlias ? true: false"
+                title="Создать документ"
+              >+</button>
+            </h6>
+
+            <List />
+          </div>
+          <div class="col-3 col-md-2 p-0 border-right">
+            <h6 class="text-center m-0 mt-2 pb-2 border-bottom">
+              Документы
+              <button
+                @click="createDoc = true; selectDocId=''; doc = {active: true, position: docs.length + 1}"
+                class="btn btn-sm btn-outline-primary p-0 pl-2 pr-2 ml-1 border-0"
+                :disabled="selectCollectionAlias ? true: false"
+                title="Создать документ"
+              >+</button>
+            </h6>
+            <List />
+          </div>
           <div class="col-6 col-md-8"></div>
         </div>
       </div>
@@ -19,10 +43,16 @@
 </template>
 
 <script>
-export default {}
+import List from '@/components/admin/List'
+
+export default {
+  components: {
+    List
+  }
+}
 </script>
 
-<style>
+<style scoped>
 .bg-image {
   background-image: url(../assets/img/insert-backdrop.webp);
   background-position: center center;
@@ -37,5 +67,14 @@ export default {}
 
 .admin-sidebar {
   height: calc(100vh - 160px);
+}
+
+.container-fluid {
+  padding: 0 12px !important;
+}
+
+.mybut {
+  display: inline-block;
+  border: none;
 }
 </style>
