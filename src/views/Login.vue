f7325bd55b3886d43ade37764e4baaf04757463b
mccrush <mccrush2027@gmail.com>
2020-07-22 19:12:18 +0700

    Setting login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -5,7 +5,7 @@
         <div class="row">
           <div class="col-12 text-left">
             <form @submit.prevent="login" class>
-              <h4 class="text-center mt-2 mb-4">Authorization</h4>
+              <h4 class="text-center mt-2 mb-3">Authorization</h4>
               <label for="email">Email</label>
               <br />
               <input
@@ -29,51 +29,115 @@
                   maxlength="20"
                 />
 
-                <div class="input-group-append">
-                  <button
-                    class="btn btn-sm btn-light p-0 pl-2 pr-2 border"
-                    type="button"
-                    @click="passType = !passType"
-                  >
-                    <!-- <img
-                      v-if="passType"
-                      src="@/assets/icons/eye-slash.svg"
-                      width="24"
-                      height="24"
-                      alt="Показать пароль"
-                      class="opacity-06"
-                    />
-                    <img
-                      v-else
-                      src="@/assets/icons/eye.svg"
-                      width="24"
-                      height="24"
-                      alt="Скрыть пароль"
-                      class="opacity-06"
-                    />-->
-                  </button>
-                </div>
+                <button
+                  class="btn btn-light p-0 pl-2 pr-2 border"
+                  type="button"
+                  @click="passType = !passType"
+                >
+                  <img
+                    v-if="passType"
+                    src="@/assets/icons/eye-slash.svg"
+                    width="24"
+                    height="24"
+                    alt="Показать пароль"
+                    class="opacity-06"
+                  />
+                  <img
+                    v-else
+                    src="@/assets/icons/eye.svg"
+                    width="24"
+                    height="24"
+                    alt="Скрыть пароль"
+                    class="opacity-06"
+                  />
+                </button>
               </div>
               <br />
               <button class="btn btn-sm btn-success btn-block" type="submit">Login</button>
             </form>
           </div>
-          <!-- <transition name="fade" mode="out-in">
-      <Message v-if="error" />
-          </transition>-->
         </div>
       </div>
     </div>
+    <!-- <transition name="fade" mode="out-in">
+      <Message v-if="error" />
+    </transition>-->
   </div>
 </template>
 
 <script>
+// import { auth } from '@/main.js'
+// import Message from '@/components/Message'
+
 export default {
   data() {
-    return {}
+    return {
+      email: '',
+      password: '',
+      passType: true,
+      error: false
+    }
   },
+  beforeMount() {
+    // if (auth.currentUser) {
+    //   this.$router.push('/admin')
+    // }
+  },
+  methods: {
+    showError() {
+      this.error = true
+      setTimeout(() => {
+        this.error = false
+      }, 4000)
+    },
+    async login() {
+      const formData = {
+        email: this.email,
+        password: this.password
+      }
 
-  methods: {}
+      if (this.email && this.password) {
+        try {
+          await this.$store.dispatch('logIn', formData)
+          this.$router.push('/admin')
+        } catch (err) {
+          this.showError()
+          if (err.code === 'auth/invalid-email') {
+            this.$store.commit('addMessage', {
+              text: 'Некорректный адрес почты!',
+              type: 'bg-danger'
+            })
+          } else if (err.code === 'auth/invalid-password') {
+            this.$store.commit('addMessage', {
+              text: 'Некорректный пароль!',
+              type: 'bg-danger'
+            })
+          } else if (err.code === 'auth/wrong-password') {
+            this.$store.commit('addMessage', {
+              text: 'Неверный пароль!',
+              type: 'bg-danger'
+            })
+          } else if (err.code === 'auth/user-not-found') {
+            this.$store.commit('addMessage', {
+              text: 'Пользователь с такой почтой не найден',
+              type: 'bg-danger'
+            })
+          } else {
+            this.$store.commit('addMessage', {
+              text: 'Ошибка: ' + err.code,
+              type: 'bg-danger'
+            })
+          }
+        } finally {
+        }
+      } else {
+        this.$store.commit('addMessage', {
+          text: 'Ошибка: поля не заполнены',
+          type: 'bg-danger'
+        })
+      }
+    }
+  }
 }
 </script>
 
@@ -85,9 +149,10 @@ export default {
 }
 
 .admin-block {
-  max-width: 240px;
-  /* height: calc(100vh - 96px); */
-  margin: 0 auto;
+  width: 240px;
+  position: absolute;
+  top: calc(50% - 170px);
+  left: calc(50% - 120px);
 }
 
 .fade-enter-active,
