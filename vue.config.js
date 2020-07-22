c89c00f46c8d6a92c23d937ba01b1ffa454a7b37
mccrush <mccrush2027@gmail.com>
2020-07-22 14:24:33 +0700

    Add vueconfig

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  productionSourceMap: false,
+
+  pwa: {
+    name: 'adminpanel',
+    themeColor: '#ffcc00',
+    manifestOptions: {
+      background_color: '#ffcc00'
+    }
+  }
+}
